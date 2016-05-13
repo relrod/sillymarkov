89fcc8da311a1dc37388df3e93ec598db0a70484
Ricky Elrod <ricky@elrod.me>
2016-05-13 14:19:00 -0400

    Fix the immediate problem of empty string, but still need to get rid of (head (head l))
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Data/SillyMarkov.hs
+++ src/Data/SillyMarkov.hs
@@ -12,7 +12,9 @@ module Data.SillyMarkov where
 import Data.List (groupBy, sort)
 import Data.List.Split (chunksOf)
 import qualified Data.Map as M
+import Data.Maybe (listToMaybe)
 import qualified Data.Text.Lazy as T
+import Safe
 
 type FTMap = M.Map T.Text (M.Map T.Text Integer)
 
@@ -28,12 +30,11 @@ table :: Functor f => (FTMap -> f FTMap) -> FrequencyTable -> f FrequencyTable
 table f (FrequencyTable a) = fmap FrequencyTable (f a)
 
 -- | Very inefficiently make a 'FrequencyTable' used for the markov chain.
-mkFrequencyTable :: T.Text -> FrequencyTable
-mkFrequencyTable = FrequencyTable . groupFreq . groupWords . splitWords
-  where
-    splitWords =
-      sort . init . chunksOf 2 . drop 1 . concatMap (replicate 2) . T.words
-    groupWords = groupBy (\x y -> head x == head y)
-    groupFreq =
-      M.fromList . map (\l -> (head (head l), (countFreq . map last $ l)))
-    countFreq x = M.fromListWith (+) [(c, 1) | c <- x]
+mkFrequencyTable :: T.Text -> Maybe FrequencyTable
+mkFrequencyTable t = do
+  sp <- initMay . chunksOf 2 . drop 1 . concatMap (replicate 2) . T.words $ t
+  let groupWords = groupBy (\x y -> listToMaybe x == listToMaybe y) . sort
+      groupFreq =
+        M.fromList . map (\l -> (head (head l), (countFreq . map last $ l)))
+      countFreq x = M.fromListWith (+) [(c, 1) | c <- x]
+  return . FrequencyTable . groupFreq . groupWords $ sp
